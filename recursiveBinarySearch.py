0f9b65d2b72db9d3f01db5156a755a552c7464dc
Rildian <130923873+Rildian@users.noreply.github.com>
2024-05-03 15:06:53 -0300

    Update recursiveBinarySearch.py

--- recursiveBinarySearch.py
+++ recursiveBinarySearch.py
@@ -7,8 +7,8 @@ def recursiveBinarySearch(arr, low, high, item):
     elif arr[mid] < item:
         return recursiveBinarySearch(arr, mid + 1, high, item)
     else:
-        return - 1 
+        return - 1 # if element not found
 
 
-array = [2, 3, 4, 10, 40] 
+array = [2, 3, 4, 10, 40] # only works if sorted
 print(recursiveBinarySearch(array, 0, len(array) - 1, 10))
